302e1d39a7e511c8bf00c61386449c67d9a80b91
ecortez3 <eduardocortez1@gmail.com>
2017-01-12 21:40:29 -0600

    Messing with posting

--- src/main/java/personal/tempesieve/api/controller/ForecastController.java
+++ src/main/java/personal/tempesieve/api/controller/ForecastController.java
@@ -1,7 +1,11 @@
 package personal.tempesieve.api.controller;
 
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * $Author: ecortez
@@ -12,5 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/forecast")
 public class ForecastController {
 
+	//Endpoint for getting forecast
+	// "/api/72347b0b78ed72b4/forecast/q/IL/Chicago.json"
 
+	@RequestMapping(value = "/")
+	public String getForecast(){
+		RestTemplate rest = new RestTemplate();
+		
+
+		HttpMessageConverter formHttpMessageConverter = new FormHttpMessageConverter();
+		HttpMessageConverter stringHttpMessageConverternew = new StringHttpMessageConverter();
+		rest.setMessageConverters(new HttpMessageConverter[]{formHttpMessageConverter, stringHttpMessageConverternew});
+
+		return postForObject(getRestTemplate(), getDomain(), location);
+	}
 }
